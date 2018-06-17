ad2c9eed982764a856a6be55bf07d40a4b2a3a51
leafyshark <taylor-lindores@hotmail.co.uk>
2018-06-17 23:56:12 +0100

    Ready for production

--- src/server/express.js
+++ src/server/express.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const server = express(); // development server setup
+const isProd = process.env.NODE_ENV === "production"
 
-const webpack = require("webpack");
-const config = require("../../config/webpack.dev.js");
-const compiler = webpack(config);
+if (!isProd) {
+  const webpack = require("webpack")
+  const config = require("../../config/webpack.dev.js")
+  const compiler = webpack(config)
 
-const webpackDevMiddleware = require("webpack-dev-middleware")(
-  compiler,
-  config.devServer
-);
-const webpackHotMiddleware = require("webpack-hot-middleware")(compiler, config.devServer);
-server.use(webpackDevMiddleware);
-server.use(webpackHotMiddleware);
+  const webpackDevMiddleware = require("webpack-dev-middleware")(
+    compiler,
+    config.devServer
+  )
 
-// const staticMiddleware = express.static("dist"); // takes one argument, the root of our web server
-// server.use(staticMiddleware); // serves up the dist folder
+  const webpackHotMiddlware = require("webpack-hot-middleware")(
+    compiler,
+    config.devServer
+  )
+
+  server.use(webpackDevMiddleware)
+  server.use(webpackHotMiddlware)
+  console.log("Middleware enabled")
+}
 
 const expressStaticGzip = require('express-static-gzip')
 server.use(expressStaticGzip("dist"))
 
-
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
-  console.log(`Server is listening on https://localhost:${PORT} in ${process.env.NODE_ENV} mode`);
-}); // development server setup
+  console.log(
+    `Server listening on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
+  )
+})
