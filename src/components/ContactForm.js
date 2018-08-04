34dec8ab144ada993595fd530341c07b6efc4a16
leafyshark <taylor-lindores@hotmail.co.uk>
2018-08-04 23:38:11 +0100

    Heroku push

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,7 +4,7 @@ import { hot } from 'react-hot-loader';
 class ContactForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { formSent: false, name: '', email: '', message: '' };
+        this.state = { formSent: false, formNotSent: false, name: '', email: '', message: '' };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
@@ -29,9 +29,8 @@ class ContactForm extends Component {
         }).then(response => {
             if (response.redirected) {
                 this.setState({ formSent: true })
-                reset()
             }
-            else this.setState({ formSent: false })
+            else this.setState({ formNotSent: true })
         }).catch((error) => {
             console.error(error);
         });
@@ -42,15 +41,15 @@ class ContactForm extends Component {
             <form className="contact" onSubmit={this.handleSubmit}>
                 <div className="contact__container">
                     <label htmlFor="name"><h5 className="contact__label contact__label--name">Name*</h5></label>
-                    <input type="text" className="contact__name contact__input" onChange={el => this.setState({ name: el.target.value })} ref={this.name} required />
+                    <input type="text" className="contact__name contact__input" onChange={el => this.setState({ name: el.target.value })} required />
                 </div>
                 <div className="contact__container">
                     <label htmlFor="email"><h5 className="contact__label">Email*</h5></label>
-                    <input type="email" className="contact__email contact__input" onChange={el => this.setState({ email: el.target.value })} ref={this.email} required />
+                    <input type="email" className="contact__email contact__input" onChange={el => this.setState({ email: el.target.value })} required />
                 </div>
                 <div className="contact__container">
                     <label htmlFor="message"><h5 className="contact__label">Message*</h5></label>
-                    <textarea rows="10" cols="30" type="input" className="contact__message contact__input" onChange={el => this.setState({ message: el.target.value })} ref={this.msg} required />
+                    <textarea rows="10" cols="30" type="input" className="contact__message contact__input" onChange={el => this.setState({ message: el.target.value })} required />
                 </div>
                 <input className='contact--btn' type="submit" value={this.state.formSent ? "Message Sent" : "Submit" || this.state.formNotSent === "Unable To Send Message"} disabled={this.state.formSent} />
             </form>
