b25ae4bc361bfcff100a0b883d69e71e278cf34b
leafyshark <taylor-lindores@hotmail.co.uk>
2020-04-07 00:48:14 +0100

    make it better 2

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -24,10 +24,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-transform-runtime"],
-        },
       },
       {
         test: /\.sass$/,
